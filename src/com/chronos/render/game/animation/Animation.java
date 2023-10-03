d11ad9d828d43f986448f5480ccfdbae91344d3b
imma <jairjair511@gmail.com>
2023-10-03 12:29:39 -0400

    cleaned up code

--- src/com/chronos/render/game/animation/Animation.java
+++ src/com/chronos/render/game/animation/Animation.java
@@ -6,30 +6,34 @@ import com.chronos.render.game.Tilemap;
 
 public class Animation extends Tilemap {
     private double lastCallTime;
-    float y, x;
-    int fps;
+    private float progress;
+    private int fps;
 
     public Animation(String path, int cols, int rows, int fps) throws ImageNotFoundException {
         super(path, cols, rows);
         lastCallTime = 0;
-        y = 0;
-        x  = 0;
+        progress = 0;
         this.fps = fps;
     }
 
     public void reset() {
-        y = 0;
-        x = 0;
+        progress = 0;
     }
 
     public Sprite getSprite() {
         double currentTime = System.nanoTime() / 1e9; // Convert nanoseconds to seconds
         double timeSinceLastCall = (lastCallTime == 0) ? 0 : currentTime - lastCallTime;
-        y+=(float)timeSinceLastCall * fps;
-        if ((int)y % getCols() == getCols() - 1)
-            x++;
 
-        lastCallTime = currentTime; // Update last call time for next invocation
-        return super.getSprite((int)x%getRows(), (int)y%getCols());
+        // Calculate the interpolation factor (t) based on time and fps
+        float t = (float) (timeSinceLastCall * fps);
+
+        progress += t;
+
+        // Update last call time for next invocation
+        lastCallTime = currentTime;
+
+        int frameIndex = (int) (progress) % (getCols() * getRows());
+
+        return super.getSprite(frameIndex / getCols(), frameIndex % getCols());
     }
 }
